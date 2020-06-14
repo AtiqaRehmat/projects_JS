d52c76fe8ebc396a844c70d410dd22fab502db69
AtiqaRehmat <66905432+AtiqaRehmat@users.noreply.github.com>
2020-06-14 18:13:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,348 @@
+ //                 Ch # 1 ALERT TASK 1
+ 
+ //alert("Error! please enter valid pasword");              
+
+
+
+ //                 Ch # 1 ALERT TASK 3
+
+//alert("Welcome to Js land..." +"\n"+ "Happy  codding!");                 
+
+
+
+ //                 Ch # 1 ALERT TASK 4
+
+//alert("Welcome to Js land...");                                 
+//alert("Happy  codding!");
+
+
+ //                 Ch # 1 ALERT TASK 5
+
+//document.write("Hello.... I can run Js throug my web browser's console");
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 1
+
+// var username = ("ATIQA   ");
+//alert(username);
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 2
+
+ //var name =("REHMAT");
+ //alert(name);
+ // alert(username + name);
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 3
+
+
+//var message=("Hello world");
+//alert(message);
+ 
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 4
+
+// var name =("Atiqa");
+//alert(name);
+
+//var age=("21 years old");
+//alert(age);
+
+//var qualification=("BS IT Undergraduate");
+//alert(qualification);
+
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 5
+
+//var line1=("PIZZA");
+//var line2=("PIZZ");
+//var line3=("PIZ");
+//var line4=("PI");
+//var line5=("P");
+//alert(line1+"\n"+line2+"\n"+line3+"\n"+line4+"\n"+line5);
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 6
+
+//var message =("My email address is : ");
+//var email =("atiqa@gimal.com");
+//alert(message+email);
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 7
+
+//var book = ("A smarter way to learn JavaScript ");
+//alert(book);
+
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 8
+
+//document.write("Yah! I can write HTML content through JavaScript");
+
+
+
+
+
+ //                 Ch # 2 VARIABLES FOR STRING TASK 9
+
+ //alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+
+
+
+
+
+  //                 Ch # 3 VARIABLES FOR NUMBERS TASK 1
+
+//var age = ("21 years old");
+//alert(age);
+
+
+
+  //                 Ch # 3 VARIABLES FOR NUMBERS TASK 2
+
+ // var visit = ("you visit");
+ // visit =1++;
+ // alert(visit);
+
+
+
+   //                 Ch # 3 VARIABLES FOR NUMBERS TASK 3
+
+//var birthYear=("My birth year is 1998 <br> data type of my deaclared variable is number ");
+//document.write(birthYear);
+
+
+
+
+   //                 Ch # 3 VARIABLES FOR NUMBERS TASK 4
+
+ //  var visitorName=("<strong>Atiqa </strong>");
+//var productTitle=(" ordered");
+//var quantity=(" <strong> 5 T-shirt </strong>");
+//document.write(visitorName+ productTitle +  quantity + "(s) on XYZ Clothing store");
+
+
+
+
+   //                 Ch # 4 VARIABLES NAME LEGAL AND ILLEGAL TASK 1
+
+ //  var name = "atiqa";
+ //  var name = "atiqa";
+// var name = "atiqa";
+
+
+
+//                 Ch # 4 VARIABLES NAME LEGAL AND ILLEGAL TASK 2
+
+//var name1;
+//var asianCountry;
+//var tiTle;
+//var Roll_No;
+//var StudentNo1;
+
+
+
+//var 2name;
+//var @asianCountry;
+//var Roll No;
+//var 123;
+//var Class^no^3
+
+
+
+
+
+//                 Ch # 4 VARIABLES NAME LEGAL AND ILLEGAL TASK 3
+
+//var heading = ("<h1>Rules for naming JS variables</h1>");
+//document.write(heading);
+//var name = ("atiqa");
+//var age = ("21");
+//var qualification = ("bs IT undergraduate");
+//document.write("<h4>Write your name age and qualification "+name +" , "+age+" , "+qualification+" , " +"</h4>");
+//document.write("<h4> Variables must begin with a ______, ______ ,_____. For example $name, _name or name </h4>");
+//document.write("<h4> Variable names are case _________</h4>");
+//document.write("<h4>Variable names should not be JS _________ </h4>");
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 1
+
+//var num1 = 5, num2 = 3, sum;
+//sum=5+3;
+//document.write("sum of 5 + 3 is "+sum );
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 2
+
+//var num1 = 5, num2 = 3, sum;
+//sum=5+3;
+//sub=5-3;
+//div=5/3;
+//multi=5*3;
+//modula=5%3;
+//document.write("sum of 5 + 3 is "+sum +"<br>");
+//document.write("subtraction of 5 - 3 is "+sub+ "<br>");
+//document.write("division of 5 / 3 is "+div+ "<br>" );
+//document.write("multiplication of 5 X 3 is "+multi+ "<br>" );
+//document.write("modula of 5 % 3 is "+modula + "<br>");
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 3
+
+//var num1 = 5;
+//var num2;
+//num2=12%6;
+//document.write("<h2>Value after variable declaration is undefined </h2>");
+//document.write("<h4> Initial value : "+(num1++)+"</h4>");
+//document.write("<h4> value after increament is : "+(num1)+"</h4>");
+//document.write("<h4> value after addition is : "+(num1+7)+"</h4>");
+//document.write("<h4> value after decreament is : "+(num1+6)+"</h4>");
+//document.write("<h4> the reminder is : "+num2+"</h4>");
+
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 4
+
+//var OneticketPrice=600;
+//var  FiveticketPrice=5;
+//var total;
+//total=FiveticketPrice*OneticketPrice;
+//document.write("<h1>Total cost of buy 5 tickets to a movie is "+total+" PKR</h1>");
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 5
+
+//var i;
+//document.write("<h1><u>TABLE OF 7<u></h1>")
+//for( i=1; i<=10; i++)
+//{
+//document.write("7 "+" X "+(i)+" = "+(7*i)+"<br>");
+//}
+
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 6
+
+//var celsius, fahrenheit;
+//celsius=(70-32)*5/9;
+//fahrenheit=(25*9/5)+32;
+//document.write("25 celsius ="+fahrenheit+"<br>");
+//document.write("70 fahrenheit ="+celsius);
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 7
+
+//var item1Price=650;
+//var quantity1=3;
+//var item2Price=100;
+//var quantity2=7;
+//var shippingCharges=100;
+//var total;
+//total=(item1Price*3)+(item2Price*quantity2)+shippingCharges
+//document.write("Price of 1 item ="+item1Price+"<br>");
+//document.write("Quantity of 1 item ="+quantity1+"<br>");
+//document.write("Price of 2 item ="+item2Price+"<br>");
+//document.write("Quantity of 2 item ="+quantity2+"<br>");
+//document.write("Shipping charges ="+shippingCharges+"<br>");
+//document.write("Total cost of our order ="+total+"<br>");
+
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 8
+
+//var totalMarks=980, obtained=804, per;
+//per=(obtained/totalMarks)*100;
+//document.write("<h1>MARKSHEET</h1><br>")
+//document.write("Toatal marks = "+totalMarks+"<br>");
+//document.write("Obtained marks = "+obtained+"<br>");
+//document.write("Your percentage = "+per+"<br>");
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 9
+
+//var  USDollar = 104.80;
+//var   SaudiRiyal = 28;
+//sum=USDollar+SudiRiyal;
+//document.write("<h1>Currency In Pakistan</h1>");
+//document.write(sum);
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 10
+
+//var num=2+5*10/2;
+
+//document.write(num);
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 11
+
+//var currentyear=2016;
+//var birthYear=1992;
+//var age=(currentyear-birthYear);
+//document.write("<h1>AGE CALCULATOR</h1>");
+//document.write("<h3>CURRENT YEAR = "+currentyear+"</h3>");
+//document.write("<h3>BIRTH YEAR = "+birthYear+"</h3>");
+//document.write("<h3>AGE = "+age+"</h3>");
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 12
+
+//var pi=3.142;
+//var radius=20;
+//var cir,area;
+//cir=(2*pi*radius);
+//area=(pi*radius*radius);
+//document.write("<h1>The Geometrizer</h1>");
+//document.write("<h1>Radius of a circle = 20</h1>");
+//document.write("<h1>The circumferece = "+cir+"</h1>");
+//document.write("<h1>The area ="+area+"</h1>");
+
+
+
+
+
+//                 Ch # 5 MATH EXPRESSIONS  TASK 13
+
+var favoritesnack=
